595ebdff2ec7696109e202f25ecbab94c259599a
t12isha <32788682+t12isha@users.noreply.github.com>
2017-10-25 21:26:12 -0700

    Update demo_script part C and part D

--- demo_script.pl
+++ demo_script.pl
@@ -39,3 +39,66 @@ kidney_score(recipient_7, K).
 % the recipient, in descending order of compatibility.
 sort_pairs(recipient_7, Sorted).
 
+
+
+% Test Queries for Section C: Ranking the recipients based on their priority score 
+
+% Rubric for calculating priority score:
+
+% 1.
+% Priority points given to recipients who are pediatric patients:
+% ?- pediatric_recipient(recipient_7, X).
+
+% 2.
+% Priority points given to recipients who were once a donor:
+% ?- once_a_donor(recipient_7, X).
+
+% 3.
+% Priority points given to a recipient's PRA percentage:
+% ?- pra_match(recipient_7, X).
+
+% 4.
+% Based on the three criteria above, calculate the total priority score for a given recipient:
+% ?- recipient_score(recipient_7, X).
+
+% 5.
+% Get list of all recipients in the dataset:
+% ?- set_prolog_flag(answer_write_options,[max_depth(0)]). 
+% ?- findall(ID, recipient(ID), R).
+% R = RecipientsIDList 
+% R = [recipient_1,recipient_2,recipient_3,recipient_4,recipient_5,recipient_6,recipient_7,recipient_8,recipient_9,recipient_10,recipient_11,recipient_12,recipient_13,recipient_14,recipient_15,recipient_16,recipient_17,recipient_18,recipient_19,recipient_20,recipient_21,recipient_22,recipient_23,recipient_24,recipient_25,recipient_26,recipient_27,recipient_28,recipient_29,recipient_30].
+
+% Score each recipient in the test data.
+% ?- scoreRecipient([recipient_1,recipient_2,recipient_3,recipient_4,recipient_5,recipient_6,recipient_7,recipient_8,recipient_9,recipient_10,recipient_11,recipient_12,recipient_13,recipient_14,recipient_15,recipient_16,recipient_17,recipient_18,recipient_19,recipient_20,recipient_21,recipient_22,recipient_23,recipient_24,recipient_25,recipient_26,recipient_27,recipient_28,recipient_29,recipient_30], X).
+
+% X = recipient list with score in front of each recipient 
+% X = [0-recipient_1,0-recipient_2,2-recipient_3,7-recipient_4,1-recipient_5,2-recipient_6,1-recipient_7,1-recipient_8,1-recipient_9,-1-recipient_10,-1-recipient_11,4-recipient_12,-1-recipient_13,6-recipient_14,3-recipient_15,1-recipient_16,-1-recipient_17,-1-recipient_18,8-recipient_19,2-recipient_20,0-recipient_21,1-recipient_22,3-recipient_23,1-recipient_24,1-recipient_25,1-recipient_26,5-recipient_27,1-recipient_28,2-recipient_29,5-recipient_30].
+
+% 6.
+% Finally, sort the scored recipient list in descending order:
+% ?- rank_recipients([recipient_1,recipient_2,recipient_3,recipient_4,recipient_5,recipient_6,recipient_7,recipient_8,recipient_9,recipient_10,recipient_11,recipient_12,recipient_13,recipient_14,recipient_15,recipient_16,recipient_17,recipient_18,recipient_19,recipient_20,recipient_21,recipient_22,recipient_23,recipient_24,recipient_25,recipient_26,recipient_27,recipient_28,recipient_29,recipient_30], X).
+
+% X = the sorted ranked list
+% X = [recipient_19,recipient_4,recipient_14,recipient_30,recipient_27,recipient_12,recipient_23,recipient_15,recipient_6,recipient_3,recipient_29,recipient_20,recipient_9,recipient_8,recipient_7,recipient_5,recipient_28,recipient_26,recipient_25,recipient_24,recipient_22,recipient_16,recipient_21,recipient_2,recipient_1,recipient_18,recipient_17,recipient_13,recipient_11,recipient_10].
+
+
+
+% Test Queries for Section D: Matching the recipients to kidneys based on the rankings of sections B and C.
+
+% List of available kidneys: [kidney_1,kidney_2,kidney_3,kidney_4,kidney_5,kidney_6,kidney_7,kidney_8,kidney_9,kidney_10].
+
+% 1.
+% Helper function match(RankedRecipientIDList, ListOfAvailableKidneys, matchedPairList).
+% ?- match([recipient_19,recipient_4,recipient_14,recipient_30,recipient_27,recipient_12,recipient_23,recipient_15,recipient_6,recipient_3,recipient_29,recipient_20,recipient_9,recipient_8,recipient_7,recipient_5,recipient_28,recipient_26,recipient_25,recipient_24,recipient_22,recipient_16,recipient_21,recipient_2,recipient_1,recipient_18,recipient_17,recipient_13,recipient_11,recipient_10], [kidney_1,kidney_2,kidney_3,kidney_4,kidney_5,kidney_6,kidney_7,kidney_8,kidney_9,kidney_10] , X).
+
+% X = [recipient_19-kidney_6,recipient_4-kidney_10,recipient_14-kidney_3,recipient_27-kidney_7,recipient_12-kidney_1,recipient_15-kidney_9,recipient_29-kidney_5,recipient_20-kidney_4,recipient_7-kidney_2,recipient_5-kidney_8].
+
+% It gives the final answer - the final matching between each kidney and its highest ranked recipient in the test data.
+
+% 2.
+% The main function match(OutputPairList) is convenient because to make the query on the final matching list,
+%   you just need to query match(X).
+
+% ?- match(X).
+
+
